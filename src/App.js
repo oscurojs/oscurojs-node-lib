43af7114c3ab34c29b5fa45a7512bad209cb69fc
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2019-11-03 20:04:55 +0100

    Added App logic for messaging
    Added a functional test for app to test out the logic.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@
  * App Abstraction Layer.
  * Oscuro JS NodeJS library 2019
  */
+const net = require('net');
+const readline = require('readline');
 
 /**
  * Default socket port where OscuroJS should be listening.
  */
 const DEFAULT_APP_PORT = 7899;
+
+// Symbols for private fields.
+const _state = Symbol('state');
+const _reader = Symbol('reader');
+const _socket = Symbol('socket');
+const _delimiter = Symbol('delimiter');
+
+/**
+ * The default delimiter used by the app to exchange messages.
+ */
+const DEFAULT_ARG_DELIMITER = ' ';
 
 /**
  * The app wrapper represents a connection abstraction between the backend (App
@@ -18,18 +31,54 @@ class App {
      * Creates the default instance of the application, running at the default
      * port.
      */
-    static createDefault() {
-        return new App(DEFAULT_APP_PORT);
+    static createDefault(delimiter = DEFAULT_ARG_DELIMITER) {
+        return new App(DEFAULT_APP_PORT, delimiter);
     }
 
     /**
      * @param {number} port The socket port where OscuroJS is running.
      */
     constructor(port) {
-        Object.defineProperty(this, 'port', {
-            get: () => port
-        });
+        const socket = net.connect({ port });
+        this[_socket] = socket;
+        const reader = readline.createInterface(socket);
+        this[_state] = 'connecting';
+        socket.once('connect', () => this[_state] = 'connected');
+        socket.once('end', () => this[_state] = 'disconnected');
+        this[_reader] = reader;
+        this[_delimiter] = DEFAULT_ARG_DELIMITER;
+    }
+    
+    /**
+     * Current app state, wether is 
+     * @type {'connecting'|'connected'|'disconnected'}
+     */
+    get state() {
+        return this[_state];
+    }
+
+    /**
+     * Calls the fn callback when the app receives a message.
+     * @param {(message: string[]) => void} fn 
+     */
+    onMessage(fn) {
+        this[_reader].on('line', line => fn(line.split(this[_delimiter])));
+    }
+
+    /**
+     * Calls the function once the client is connected.
+     * @param {() => void} fn 
+     */
+    onceReady(fn) {
+        this[_socket].once('connect', fn);
+    }
+
+    /**
+     * Disconnects from the host.
+     */
+    disconnect() {
+        this[_socket].end();
     }
 }
 
-module.exports = { App, DEFAULT_APP_PORT };
+module.exports = { App, DEFAULT_APP_PORT, DEFAULT_ARG_DELIMITER };
